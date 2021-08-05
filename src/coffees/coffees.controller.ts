79430c75096fb0bc62d0c6b9f410a9342845e179
Lee Sang-Min <sangmin4208@gmail.com>
2021-08-05 22:08:18 +0900

    Handling Update and Delete

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,17 +1,30 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
     @Get()
-    findAll(){
-        return "This action returns all coffees"
+    findAll() {
+      return 'This action returns all coffees';
     }
+  
     @Get(':id')
-    findOne(@Param('id') id){
-        return `This action returns ${id} coffee`
+    findOne(@Param('id') id: string) {
+      return `This action returns #${id} coffee`;
     }
+  
     @Post()
-    Create(@Body() body) {
-        return body 
+    create(@Body() body) {
+      return body;
+      // return `This action creates a coffee`;
+    }
+  
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+      return `This action updates #${id} coffee`;
+    }
+  
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+      return `This action removes #${id} coffee`;
     }
 }
