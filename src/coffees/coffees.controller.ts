f32d8f9c58d8a3f0c8e56223ea72d6e716d70465
Lee Sang-Min <sangmin4208@gmail.com>
2021-08-13 09:17:30 +0900

    Auto-transform Payloads to DTO instances

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -24,12 +24,13 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coffeesService.findOne(id);
+  findOne(@Param('id') id: number) {
+    return this.coffeesService.findOne(""+id);
   }
 
   @Post()
-  create(@Body() createCoffeeDto:CreateCoffeeDto) {
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto)
     return this.coffeesService.create(createCoffeeDto);
   }
 
