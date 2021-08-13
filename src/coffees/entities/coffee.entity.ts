929598c3f17ffa727509dd5a1e5ba97b1cfa0852
Lee Sang-Min <sangmin4208@gmail.com>
2021-08-13 10:33:16 +0900

    TypeORM

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity() //sql table ==='coffee' 괄호안에 이름넣으면 그 이름으로 테이블생성
 export class Coffee{
+    @PrimaryGeneratedColumn()
     id:number;
+    @Column()
     name: string;
+    @Column()
     brand: string;
+    @Column('json', {nullable:true})
     flavors: string[];
 }
